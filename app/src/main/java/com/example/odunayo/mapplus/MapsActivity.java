78b60c0b8ed6d0a03f3b67ff48bd6bbf76d0043b
Odunayo Kusoro <okusoro@princeton.edu>
2015-03-27 13:42:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/odunayo/mapplus/MapsActivity.java
@@ -0,0 +1,176 @@
+package com.example.odunayo.mapplus;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LocationManager locationManager;  // location manager
+    private LocationListener locationListener;
+    private String provider;                  // either GPS or Network
+    private Location location;                // stores location of user
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+
+        // initialize locationManager
+        final LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        //locationListener = new Location
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        // set camera to roughly the area of location
+        location = locationManager.getLastKnownLocation(provider);
+        location = mMap.getMyLocation();
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+
+                // Getting latitude of the current location
+                double latitude = location.getLatitude();
+
+                // Getting longitude of the current location
+                double longitude = location.getLongitude();
+
+                // Creating a LatLng object for the current location
+                LatLng latLng = new LatLng(latitude, longitude);
+
+                // Showing the current location in Google Map
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Zoom in the Google Map
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+                // stop
+                locationManager.removeUpdates(this);
+                // redraw the marker when get location update.
+                drawMarker(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+        locationManager.requestLocationUpdates(provider, 20000, 0, locationListener);
+
+            if (location == null) {
+                Toast.makeText(getApplicationContext(), "fuck", Toast.LENGTH_LONG).show();
+               // locationListener.onLocationChanged(location);
+            }
+
+
+    }
+
+    private void drawMarker(Location location){
+        // Remove any existing markers on the map
+        mMap.clear();
+        LatLng currentPosition = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(currentPosition)
+                .snippet("Lat:" + location.getLatitude() + "Lng:"+ location.getLongitude())
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("ME"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+
+        location = mMap.getMyLocation();
+        if (location == null)
+            Toast.makeText(getApplicationContext(), "Loc is null", Toast.LENGTH_LONG).show();
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+       mMap.setMyLocationEnabled(true);
+       Location g =  locationManager.getLastKnownLocation(provider);
+
+       //location =  mMap.getMyLocation();
+        double lat = 0;
+        double lon = 0;
+        if (location != null) {
+            Toast.makeText(getApplicationContext(), "fasdd", Toast.LENGTH_LONG).show();
+             lat = location.getLatitude();
+             lon = location.getLongitude();
+        }
+        LatLng myArea = new LatLng(lat, lon);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myArea, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Here")
+                .snippet("You Are Here")
+                .position(myArea));
+    }
+
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+
+        // show location
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+      //  onMapReady(mMap);
+    }
+}
