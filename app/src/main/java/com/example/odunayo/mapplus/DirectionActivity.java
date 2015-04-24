fd2da8b1ad29fc5c18e26421372d9dee2f43637b
pqde <pqde@princeton.edu>
2015-04-24 13:20:23 -0400

    Added multiple destinations on Directions

--- app/src/main/java/com/example/odunayo/mapplus/DirectionActivity.java
+++ app/src/main/java/com/example/odunayo/mapplus/DirectionActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 
@@ -14,10 +16,52 @@ public class DirectionActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE_FROM = "com.example.peter.tigermapsui.MESSAGEFROM";
     public final static String EXTRA_MESSAGE_TO = "com.example.peter.tigermapsui.MESSAGETO";
 
+    private EditText[] added = new EditText[5];
+    private int index = 0;
+    private LinearLayout layout;
+    private Button addEditText;
+    private Button removeEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direction);
+
+        layout=(LinearLayout)findViewById(R.id.layout);
+
+        addEditText = (Button) findViewById(R.id.addEditBut);
+        removeEditText = (Button) findViewById(R.id.removeEditBut);
+        removeEditText.setEnabled(false);
+
+        added[0] =(EditText) findViewById(R.id.addedEdit1);
+        added[1] =(EditText) findViewById(R.id.addedEdit2);
+        added[2] =(EditText) findViewById(R.id.addedEdit3);
+        added[3] =(EditText) findViewById(R.id.addedEdit4);
+        added[4] =(EditText) findViewById(R.id.addedEdit5);
+
+        addEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //added[index].setId("added[index]" + "layout".getChildCount()+1);
+                added[index].setVisibility(View.VISIBLE);
+                index++;
+                //if index > 0, restore clickability to removeEdit
+                if(index > 0) removeEditText.setEnabled(true);
+                //if index == 5 remove clickability of addEdit
+                if(index == 5) addEditText.setEnabled(false);
+            }
+        });
+
+        removeEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                added[index-1].setVisibility(View.GONE);
+                index--;
+                //if index == 0, remove clickability
+                if(index == 0) removeEditText.setEnabled(false);
+                if(index < 5) addEditText.setEnabled(true);
+            }
+        });
     }
 
 
@@ -28,6 +72,27 @@ public class DirectionActivity extends ActionBarActivity {
         return true;
     }
 
+    /*
+    public void addEdit(View view) {
+        added[index] =new EditText(this);
+        added[index].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(added[index]);
+        index++;
+        //if index > 0, restore clickability to removeEdit
+        if(index > 0) removeEditText.setEnabled(true);
+        //if index == 5 remove clickability of addEdit
+        if(index == 5) addEditText.setEnabled(false);
+    } */
+    /*
+    public void removeEdit(View view) {
+        layout.removeView(added[index-1]);
+        index--;
+        //if index == 0, remove clickability
+        if(index == 0) removeEditText.setEnabled(false);
+        if(index < 5) addEditText.setEnabled(true);
+    }
+*/
     public void direcsend(View view) {
         Intent intent = new Intent(this, DisplayDirectionActivity.class);
         EditText editText = (EditText) findViewById(R.id.fromdest);
@@ -63,6 +128,13 @@ public class DirectionActivity extends ActionBarActivity {
 
         EditText editTextto = (EditText) findViewById(R.id.todest);
         String tomessage = editTextto.getText().toString();
+
+        for(int i = 0; i < added.length; i++) {
+            if(added[i].getVisibility() == View.VISIBLE) {
+                tomessage = tomessage.concat(", " + added[i].getText().toString());
+            }
+        }
+
         intent.putExtra(EXTRA_MESSAGE_TO, tomessage);
 
         Toast.makeText(getApplicationContext(),"To =" + tomessage, Toast.LENGTH_SHORT).show();
