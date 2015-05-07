b197dde45a1b2bafccb7c72aac613e8491efef95
Harsan Sidhu <YOUR EMAIL ADDRESS>
2015-05-07 14:17:21 -0400

    UI Changes

--- app/src/main/java/com/example/odunayo/mapplus/Splash.java
+++ app/src/main/java/com/example/odunayo/mapplus/Splash.java
@@ -3,5 +3,23 @@ package com.example.odunayo.mapplus;
 /**
  * Created by harsansidhu on 5/7/15.
  */
-public class Splash {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 3000);
+    }
 }
